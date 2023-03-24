e3da892338d714df998adcc2141276fbd2a5018d
MahmoodKhalil57 <mahmood.khalil@technologi.co.uk>
2023-03-24 22:48:10 +0000

    remove useless reduce

--- src/branchData.ts
+++ src/branchData.ts
@@ -7,20 +7,9 @@ let getRawBranchData = async(targetRepo:string) => {
 let getformattedRawBranchData = async(rawData: string) => {
   let branchData = rawData.split("\n").slice(0, -1)
 
-  // let branchDataObj = branchData.map( branch => {
-  //   let branchArr = branch.split(" ")
-  //   let branchObj = {
-  //     branchName : branchArr[0],
-  //     commitHash: branchArr[1].slice(0, 7),
-  //     upstream: branchArr[2],
-  //     upstreamTrack: branchArr[3],
-  //     head: branchArr[4]
-  //   }
-  //   return branchObj
-  //   }
-  // )
-  // remove all branches that does not contain branchName = origin/ and then remove the origin/ part
-  let branchDataObj = branchData.reduce( (acc, branch) => {
+  type branchDataObjType = {branchName: string, commitHash: string, upstream: string, upstreamTrack: string, head: string}[]
+  let branchDataObj: branchDataObjType =  []
+  branchData.forEach( (branch) => {
     let branchArr = branch.split(" ")
     let branchName = branchArr[0]
     if(branchName.includes("origin/")){
@@ -31,10 +20,9 @@ let getformattedRawBranchData = async(rawData: string) => {
         upstreamTrack: branchArr[3],
         head: branchArr[4]
       }
-      acc.push(branchObj)
+      branchDataObj.push(branchObj)
     }
-    return acc
-  }, [] as {branchName: string, commitHash: string, upstream: string, upstreamTrack: string, head: string}[])
+  })
 
   return branchDataObj
 }
